811259957bb07fd5f9571126fa4db1a4ea0453d1
Julian Celeita <andresceleita@yahoo.es>
2022-11-06 12:36:40 -0500

    Initial components and styles in GifApp

---
+++ src/GiftApp.tsx
@@ -0,0 +1,14 @@
+
+export const GifApp = () => {
+    return (
+      <>
+          {/* Title */}
+          <h1>Gif App</h1>
+  
+          {/* Input */}
+  
+          {/* Listado de Gifs */}
+              {/* Gift items */}
+      </>
+    )
+  }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { GifApp } from './GiftApp';
+import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <GifApp />
   </React.StrictMode>
 )
