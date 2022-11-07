a456782cd62651063f667876bf66269d8742fcf4
Julian Celeita <andresceleita@yahoo.es>
2022-11-07 18:59:08 -0500

    input does not work empty, the code is cleaned

--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -1,9 +1,8 @@
 import { useState } from 'react';
 
-
 export const AddCategory = ( {setCategory}:{setCategory:any} ) => {
 
-    const [inputValue, setInputValue] = useState('Ducati Panigale')
+    const [inputValue, setInputValue] = useState('')
 
     const onInputChange = ({target}:{target:any}) => {     // Va a arrojar el valor de target dentro del input
         console.log(target.value);
@@ -12,14 +11,15 @@ export const AddCategory = ( {setCategory}:{setCategory:any} ) => {
 
     const onSubmit = (event:any) => {           // Recibo el evento enviado por el formulario más abajo
         event.preventDefault();                 // Prevengo que se refresque la página al darle enter al input
-        console.log(inputValue);                // Imprimo en pantalla el valor recibido en el input (este lo necesitaré para procesarlo)
-        setCategory( (category: any) => [...category, inputValue] );
-    }
+        if ( inputValue.trim().length <= 1 ) return;    // Si el valor en el input es < o = a 1, haga un return y NO ejecuta el resto de codigo. trim remueve los espacios al inicio y al final
 
-
+        // console.log(inputValue);             // Imprimo en pantalla el valor recibido en el input (este lo necesitaré para procesarlo)
+        setCategory( (category: any) => [...category, inputValue] );      // Arrojo el resultado para setCategory, tomo el arreglo de category y le agrego el valor puesto en el input (inputValue) 
+        setInputValue('');                      // Al final de la función, tome la casilla de input y la deje vacia
+      }
 
   return (
-    <form onSubmit={ (event) => onSubmit(event) }>     {/* Al darle submit envío el evento(una lista de caracteristicas que contiene tambien el target con un value(en este caso lo escrito en el input) a una función Onsubmit */}
+    <form onSubmit={ onSubmit }>                {/* ejecuto función submit al darle enter, al darle submit envío el evento(una lista de caracteristicas que contiene tambien el target con un value(en este caso lo escrito en el input) a una función Onsubmit */}
         <input 
         type="text"
         placeholder="Buscar Gifs" 
