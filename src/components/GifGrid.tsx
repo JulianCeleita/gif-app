c0b25bd70703a351e899161cdc6557e131e856ab
Julian Celeita <andresceleita@yahoo.es>
2022-11-08 20:47:16 -0500

    id and title extracted from images in an array

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -1,19 +1,32 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { getGifs } from '../helpers/getGifs';
 
 // Ejecutamos el funcional component pero no debe ir dentro ninguna petición de http porque sino va a ocupar mucho espacio en memoria al estarla pidiendo
 
 export const GifGrid = ( {category}:{category:any} ) => {           // Va a arrojar el valor de category hacia el main
 
-    useEffect( () => {                              // useEffect dispara efectos secundarios: cuando el componente se renderiza, quiero generar tal efecto. Cuando la función cambia, quiero generar tal efecto...etc.
-        getGifs( category );                        // Recibo la category y ejecuto una sola vez la función para que me traiga los gifs y los meta en la category
+    const [ images, setImages ] = useState([]);
 
-    }, [] )                                         // Si [] lo dejo vacío, va a cargar el componente en consola solo la primer vez cuando se construye 
+    const getImages = async() => {
+        const newImages = await getGifs(category);      // la constante newImages tendrá los elementos de category arrojados por la función de getGifs
+        setImages(newImages);                           // setImages ahora toma el valor arrojado por nnewImages
+    }
 
+    useEffect( () => {                                  // useEffect dispara efectos secundarios: cuando el componente se renderiza, quiero generar tal efecto. Cuando la función cambia, quiero generar tal efecto...etc.
+        getImages();                                    // Incluyo ya las images en el useEffect
+    }, [] )                                             // Si [] lo dejo vacío, va a cargar el componente en consola solo la primer vez cuando se construye 
 
     return (
         <>
             <h3> {category} </h3>
+            <ol>
+                {
+                    images.map( ({ id, title }) => (                //Hago un arreglo de image y desestructuro sacando el id y el title
+                        <li key={ id }> { title } </li>             //Uso el id y el title sacados de la imagen 
+                    ))
+                }
+            </ol>
+            
         </>
     )
 }
