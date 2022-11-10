b88f0f866da49b544955f253d69c448db2a17193
Julian Celeita <andresceleita@yahoo.es>
2022-11-09 21:09:02 -0500

    Building a new hook useFetchGifs and isLoading

--- src/components/GifGrid.tsx
+++ src/components/GifGrid.tsx
@@ -1,21 +1,12 @@
-import { useEffect, useState } from 'react';
 import { GifItem } from './GifItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 // Ejecutamos el funcional component pero no debe ir dentro ninguna petición de http porque sino va a ocupar mucho espacio en memoria al estarla pidiendo
 
 export const GifGrid = ( {category}:{category:any} ) => {           // Va a arrojar el valor de category hacia el main
 
-    const [ images, setImages ] = useState([]);
+    const { images, isLoading } = useFetchGifs( category );         // Importo el hook creado y le saco las images y el isLoading
 
-    const getImages = async() => {
-        const newImages = await getGifs(category);      // la constante newImages tendrá los elementos de category arrojados por la función de getGifs
-        setImages(newImages);                           // setImages ahora toma el valor arrojado por nnewImages
-    }
-
-    useEffect( () => {                                  // useEffect dispara efectos secundarios: cuando el componente se renderiza, quiero generar tal efecto. Cuando la función cambia, quiero generar tal efecto...etc.
-        getImages();                                    // Incluyo ya las images en el useEffect
-    }, [])                                              // Si [] lo dejo vacío, va a cargar el componente en consola solo la primer vez cuando se construye 
 
     return (
         <>
---
+++ src/hooks/useFetchGifs.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from 'react';
+import { getGifs } from '../helpers/getGifs';
+
+// Hook = No es más que una función que regresa algo (return)
+
+export const useFetchGifs = ( {category}:{category:any} ) => {
+
+    const [ images, setImages ] = useState([]);
+    const [isLoading, setIsLoading] = useState( true);      // El isLoading va a ser true antes de cargar las images
+
+    const getImages = async() => {
+        const newImages = await getGifs(category);      // la constante newImages tendrá los elementos de category arrojados por la función de getGifs
+        setImages(newImages);                           // setImages ahora toma el valor arrojado por nnewImages
+        setIsLoading( false );                          // Como la imagen ya cargó arriba, el isloading se vuelve falso
+    }
+
+    useEffect( () => {                                  // useEffect dispara efectos secundarios: cuando el componente se renderiza, quiero generar tal efecto. Cuando la función cambia, quiero generar tal efecto...etc.
+        getImages();                                    // Incluyo ya las images en el useEffect
+    }, [])                                              // Si [] lo dejo vacío, va a cargar el componente en consola solo la primer vez cuando se construye 
+ 
+
+  return {
+    images,                                     // Envio las images que saqué
+    isLoading,                                  // Mando el resultado a ver si está cargando o no la imagen
+  }
+}
