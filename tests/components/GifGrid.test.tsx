e6f016e79b4ee584a86771c810051c9e05bb0590
Julian Celeita <andresceleita@yahoo.es>
2022-11-27 17:01:26 -0500

    test with useFetchGif

--- tests/components/GifGrid.test.tsx
+++ tests/components/GifGrid.test.tsx
@@ -1,7 +1,10 @@
-import { describe, expect, test } from '@jest/globals';
-import { GifGrid } from '../../src/components/GifGrid';
-import { render, screen } from '@testing-library/react';
 import React from 'react';
+import { describe, expect, jest, test } from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');              // Que haga un mock desde esa dirección
 
 describe ( 'Test in <GifGrid/>', () => {
 
@@ -9,12 +12,38 @@ describe ( 'Test in <GifGrid/>', () => {
 
     test('Must show the loading initially', () => { 
 
+        useFetchGifs.mockReturnValue({
+            images: [],
+            isLoading: true
+        })
+
         render (<GifGrid category={category} />);
         expect ( screen.getByText( 'Loading...' ))
         expect ( screen.getByText( category ))
     })
 
     test ( 'Must show item when the useFetchGifs images are loaded', () => {
+
+        const gifs = [
+            {
+                id: 'ABC',
+                title: 'Yamaha',
+                url: 'http://localhost/yamaha.jpg'
+            },
+            {
+                id: 'CDE',
+                title: 'Benelli',
+                url: 'http://localhost/benelli.jpg'
+            }
+    ]
+
+        useFetchGifs.mockReturnValue({
+            images: gifs,
+            isLoading: false
+        });
+
+        render( <GifGrid category={category} /> );
+        expect( screen.getAllByRole('img').length).toBe(2);
 
     })
 
